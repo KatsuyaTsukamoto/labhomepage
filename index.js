cb81580972bdc1aaef5264f4fec974cf49462ba1
KatsuyaTsukamoto <katsuyat9930@gmail.com>
2022-07-28 18:58:49 +0900

    1858

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-<button  onclick="alert('ボタンをクリックしました');"></button>
+{/* <button  onclick="alert('ボタンをクリックしました');"></button> */}
+function sample(){
+    document.getElementById("txt01").innerText = "変わりました";
+}
+// button onclick="sample01();">クリックしてください
